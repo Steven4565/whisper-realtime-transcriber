0b254046fae899fbde91340618da05af8bf38cd5
Steven4565 <stevenchristian4565@gmail.com>
2022-10-12 11:35:00 +0700

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Run the backend with `python -m app` because I don't know how python imports work
+
 Don't forget to `choco install ffmpeg`
 
 Install whisper through `pip install git+https://github.com/openai/whisper.git`
--- test/whispertest.py
+++
@@ -1,7 +0,0 @@
-import whisper
-
-model = whisper.load_model("tiny")
-text = model.transcribe(
-    "C:/Users/Steven/Documents/1-Computer/Competitions/OpenAIWhisperHackathon/flaskApi/resources/recording.mp3")
-# printing the transcribe
-print(text['text'])
