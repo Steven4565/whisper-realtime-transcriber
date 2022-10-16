81c1d47c6a83d9933a7193f52db097d3296059ee
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2022-10-16 10:54:27 -0700

    Update transcript.py

--- backend/api/transcript.py
+++ backend/api/transcript.py
@@ -11,12 +11,14 @@ RECORDING_PATH = os.path.join(
 class Transcript(Resource):
     def post(self):
         # get data from request
-        data = request.data.decode("utf-8")
+        data = request.data.decode('utf-8').split("base64,")
+        # data = base64.b64decode(data)
+        print(data)
 
-        # decode data into byte-array
+        # # decode data into byte-array
         # Base64Encoder.decode(RECORDING_PATH, data.split('base64,')[1])
         # whisperModelInstance.read_audio(RECORDING_PATH)
-        # transcribe the audio
+        # # transcribe the audio
         # transcript = whisperModelInstance.transcribe(RECORDING_PATH).text
 
         # return transcript
