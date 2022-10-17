ef9e5e56808dbcd41ec10d4684ea9f35776fe74c
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2022-10-16 17:40:41 -0700

    Update transcript.py

--- backend/api/transcript.py
+++ backend/api/transcript.py
@@ -2,10 +2,7 @@ from utils.WhisperModel import whisperModelInstance
 from flask import request
 from flask_restful import Resource
 from utils.encoder import Base64Encoder
-import soundfile as sf
-import io
 import os
-import base64
 
 RECORDING_PATH = os.path.join(
     os.getcwd(), 'resources', 'recording.wav')
