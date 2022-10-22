ff62741ecbcd9f2e8c06b1cbf0392ab48ac508ba
Steven4565 <stevenchristian4565@gmail.com>
2022-10-22 16:23:07 +0700

    used setInterval for timer

--- frontend/src/components/ReactMic.tsx
+++ frontend/src/components/ReactMic.tsx
@@ -2,35 +2,38 @@ import { ReactMic, ReactMicStopEvent } from 'react-mic';
 import { Component } from 'react';
 import { blobToBase64 } from '../utils/blobToBase64';
 
-export class Example extends Component<{}, {record:boolean, buffer: Array<Blob>, timestamp: number}> {
+export class Example extends Component<{}, {record:boolean, buffer: Array<Blob>, bufferLength: number}> {
   constructor(props: any) {
     super(props);
     this.state = {
       record: false,
       buffer: [],
-      timestamp: Math.floor(Date.now() /1000)
+      bufferLength: 4000,
     };
 
     this.onData = this.onData.bind(this);
     this.onStop = this.onStop.bind(this);
+    this.startRecording = this.startRecording.bind(this);
+    this.stopRecording = this.stopRecording.bind(this);
+    this.sendData = this.sendData.bind(this);
   }
 
   startRecording = () => {
-    this.setState({ record: true });
+    this.setState({ record: true});
+    setInterval(this.sendData, this.state.bufferLength);
   };
 
   stopRecording = () => {
     this.setState({ record: false });
   };
 
-  async onData(recordedBlob:Blob) {
+  onData(recordedBlob:Blob) {
     this.setState((state) => { return {
       buffer: [...state.buffer, recordedBlob]
     };});
+  }
 
-    const currentTimestamp = Math.floor(Date.now()/1000);
-    if (currentTimestamp === this.state.timestamp) return;
-    
+  async sendData(){
     const blob = new Blob(this.state.buffer, { type: "audio/wav"});
     
     fetch("http://localhost:5000/transcript", {
@@ -42,10 +45,14 @@ export class Example extends Component<{}, {record:boolean, buffer: Array<Blob>,
         body: await blobToBase64(blob)
     });
 
+    console.log(await blobToBase64(blob));
+
+    // reset buffer
     this.setState(() => { return {
-      // buffer: [],
-      timestamp: currentTimestamp
+      buffer: [],
     };}); 
+
+    if (this.state.record) setInterval(this.sendData, this.state.bufferLength);
   }
 
   async onStop(recordedBlob:ReactMicStopEvent) {
